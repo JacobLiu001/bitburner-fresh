665b28db068304091346297749a3ba7f5f52ca3f
Jacob Liu <yunzhi.liu001@outlook.com>
2024-12-29 22:27:42 +0800

    Utilities

---
+++ mirror/home/tasks/utils.ts
@@ -0,0 +1,13 @@
+export function getAllServers(ns: NS, root: string) {
+    const visited: string[] = [];
+    const stack: string[] = [root];
+    while (stack.length > 0) {
+        const current = stack.pop();
+        if (visited.includes(current)) {
+            continue;
+        }
+        visited.push(current);
+        stack.push(...ns.scan(current));
+    }
+    return visited;
+}
