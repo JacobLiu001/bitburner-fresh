17bb26d02495c75b8e042037259e2965efc3c5a1
Jacob Liu <yunzhi.liu001@outlook.com>
2025-01-08 19:45:11 +0800

    tiny util scripts

---
+++ mirror/home/tasks/write_all_servers.ts
@@ -0,0 +1,7 @@
+import { getAllServers } from "./utils";
+
+export async function main(ns: NS) {
+    const allServers = getAllServers(ns, "home");
+    ns.write("/tmp/all_servers.txt", JSON.stringify(allServers), "w");
+    ns.write("/tmp/all_rooted.txt", JSON.stringify(allServers.filter(s => ns.hasRootAccess(s))), "w");
+}
